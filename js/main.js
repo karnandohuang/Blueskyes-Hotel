65728868cd534c5099df628a2776019ce64b7ccf
karnandohuang <38078289+karnandohuang@users.noreply.github.com>
2018-05-26 16:39:57 +0700

    v2.1
    Added Hamburger Navigation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-function navFunction() {
-    var x = document.getElementById("nav");
-    if (x.className === "navs") {
-        x.className += " responsive";
-    } else {
-        x.className = "navs";
-    }
-}
+
